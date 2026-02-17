90e8bf4745b20106c5dbfde167a75bc2498cf980
sboyarkin <sergey.boyarkin@mail.ru>
2026-02-17 11:37:42 +0500

    add RTK

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,27 @@
+import {useSelector, useDispatch} from "react-redux";
+import { decrement, increment } from '../../features/counterSlice.js'
 export const MainPage = () => {
+   const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
     return(
         <>
-        MAIN
+            <div>
+              <div>
+                <button
+                  aria-label="Increment value"
+                  onClick={() => dispatch(increment())}
+                >
+                  Increment
+                </button>
+                <span>{count}</span>
+                <button
+                  aria-label="Decrement value"
+                  onClick={() => dispatch(decrement())}
+                >
+                  Decrement
+                </button>
+              </div>
+            </div>
         </>
     )
 
